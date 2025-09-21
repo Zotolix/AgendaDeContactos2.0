201b588951311257d2d008e2b839cadd943c8b8e
Zotolix <zotoconz@gmail.com>
2025-09-21 12:53:45 -0600

    Update Agenda.java

--- src/servicio/Agenda.java
+++ src/servicio/Agenda.java
@@ -27,5 +27,38 @@ public List<Contacto> buscarPorNombre(String nombre){
         }
     }
     return resultado;
+
+public List<Contacto> eliminarContacto(String telefono){
+	for (int i = 0; i<contactos.size(); i++){
+		Contacto c = contactos.get(i);
+		if(c.getTelefono().equals(telefono)){
+			return contactos.remove(i);
+		}
+	}
+	return null;
+    
+public List<Contacto> actualizarContacto(String telefono, String nuevoNombre,String nuevoApellido, String nuevoTelefono, String nuevoEmail, date nuevaFechaDeNacimiento ){
+    for (Contacto c : contactos){
+        if (c.getTelefono().equals(telefono)){
+            c.setNombre(nuevoNombre);
+            c.setTelefono(nuevoTelefono);
+            c.setApellido(nuevoApellido);
+            c.setEmail(nuevoEmail);
+            c.setFechadeNacimiento(nuevaFechaDeNacimiento);
+        }
+        else{
+            System.out.println("El contacto no se encontro o no existe ");
+        }
+    }
+    }
+public List<Contacto> verContacto(String telefono){
+    for (int i=0; i<contactos.size(); i++){
+        Contacto c = contactos.get(i);
+        if (c.getTelefono.equals(telefono)){
+            return c;
+        }
+    }
 }
 }
+}
+

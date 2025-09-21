f04b4f7d17847bc7a13bc51bfef25e31900fe313
Stephanie <Stephanie@stephanie>
2025-09-20 20:13:57 -0600

    CONTACTOS LISTO

--- src/agendacontactos/Contacto.java
+++ src/agendacontactos/Contacto.java
@@ -3,23 +3,31 @@ package agendacontactos;
 
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.*;
+import java.util.regex.Pattern;
 
 public class Contacto {
+
     private String nombre;
     private String apellidoPaterno;
     private String apellidoMaterno;
-    private Date fechaNacimiento;
+    private LocalDate fechaNacimiento;
     // calcular edad dependiendo la fecha de nacimiento 
     private String telefonoCelular;
     private String email;
+
+    public Contacto() {
+    }
     
     //Constructor
-    public Contacto(String nombre, String apellidoPaterno ,String apellidoMaterno, Date fechaNacimiento, String telefonoCelular, String email){
+    public Contacto(String nombre, String apellidoPaterno,
+            String apellidoMaterno, LocalDate fechaNacimiento,
+            String telefonoCelular, String email){
         this.nombre = nombre;
         this.apellidoPaterno = apellidoPaterno;
         this.apellidoMaterno = apellidoMaterno;
         this.fechaNacimiento = fechaNacimiento;
+        /* validaciones centralizadas. como se hace con REGEX cada que se se 
+        cree un contacto, se aplicará automáticamente**/
         this.telefonoCelular = telefonoCelular;
         this.email = email;
     }
@@ -36,7 +44,7 @@ public class Contacto {
         return apellidoMaterno;
     }
 
-    public Date getFechaNacimiento() {
+    public LocalDate getFechaNacimiento() {
         return fechaNacimiento;
     }
 
@@ -60,27 +68,40 @@ public class Contacto {
         this.apellidoMaterno = apellidoMaterno;
     }
 
-    public void setFechaNacimiento(Date fechaDeNacimiento) {
+    public void setFechaNacimiento(LocalDate fechaDeNacimiento) {
         this.fechaNacimiento = fechaDeNacimiento;
     }
 
     public void setTelefonoCelular(String telefonoCelular) {
-        this.telefonoCelular = telefonoCelular;
+        if (telefonoCelular != null && validarTelefono(telefonoCelular)){
+            this.telefonoCelular = telefonoCelular;
+        }else {
+            throw new IllegalArgumentException ("Teléfono inválido. Deben ser 10 dígitos");
+        }
     }
 
     public void setEmail(String email) {
-        this.email = email;
+        if (email != null && validarEmail(email)){
+            this.email = email;
+        }else{
+            throw new IllegalArgumentException("Email inválido");
+        }
     }
     
     //método calcular edad
-    public int calcularEdad(LocalDate fechaNacimiento){
-        if (fechaNacimiento == null){
-            return 0;
-        }
-        LocalDate hoy = LocalDate.now();
-        return Period.between(fechaNacimiento, hoy).getYears();
+    public int calcularEdad(){
+        if (fechaNacimiento == null) return 0;
+        return Period.between(fechaNacimiento, LocalDate.now()).getYears();
+    }
+    //period.between devuelve la edad a partir de la fecha de nacimiento
+    
+    public static boolean validarTelefono(String telefono){
+        return Pattern.matches("\\d{10}",telefono); //revisa si el texto cumple con el patrón
     }
     
+    public static boolean validarEmail(String email){
+        return Pattern.matches("^[A-Za-z0-9+_.-]+@(.+)$",email);
+    }
     //toString
     @Override
     public String toString(){
